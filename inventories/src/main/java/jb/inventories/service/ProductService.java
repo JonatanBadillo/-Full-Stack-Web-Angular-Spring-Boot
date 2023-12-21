d25199ba0d6a0c8333a726f27ce5b843759af0c1
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-12-21 13:04:01 -0600

    Service Layer and Data with Spring

--- inventories/src/main/java/jb/inventories/service/ProductService.java
+++ inventories/src/main/java/jb/inventories/service/ProductService.java
@@ -1,4 +1,35 @@
 package jb.inventories.service;
 
-public class ProductService {
+import jb.inventories.model.Product;
+import jb.inventories.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class ProductService implements IProductService{
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public List<Product> listProducts() {
+        this.productRepository.findAll();
+    }
+
+    @Override
+    public Product searchProductById(Integer IdProduct) {
+        Product product = this.productRepository.findById(IdProduct).orElse(null);
+        return product;
+    }
+
+    @Override
+    public void saveProduct(Product product) {
+        this.productRepository.save(product);
+    }
+
+    @Override
+    public void deleteProductById(Integer idProduct) {
+        this.productRepository.deleteById(idProduct);
+    }
 }
