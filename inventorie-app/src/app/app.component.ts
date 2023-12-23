f280ffc9aea7b1c3a0e1d8ab00395e3a20c7a683
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-12-22 21:21:03 -0600

    Lists and Service Class with Angular

--- inventorie-app/src/app/app.component.ts
+++ inventorie-app/src/app/app.component.ts
@@ -2,14 +2,23 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ProductListComponent } from "./product-list/product-list.component";
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, ProductListComponent]
+    imports: [CommonModule, RouterOutlet, ProductListComponent, HttpClientModule]
 })
 export class AppComponent {
   title = 'inventorie-app';
 }
+
+
+
+
+
+
+ 
+
--- inventorie-app/src/app/product-list/product-list.component.ts
+++ inventorie-app/src/app/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Product } from '../product';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -7,5 +9,21 @@ import { Component } from '@angular/core';
   templateUrl: './product-list.component.html',
 })
 export class ProductListComponent {
+  products: Product[];
 
+  constructor(private productService: ProductService){}
+
+  ngOnInit(){
+    // load all products
+    this.obtainProducts();
+  }
+
+  private obtainProducts(){
+    // consume data of observable
+    this.productService.obtainProductsList().subscribe(
+      (data => {
+        this.products = data;
+      })
+    );
+  }
 }
