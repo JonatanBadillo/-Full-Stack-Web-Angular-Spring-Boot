be277d1971f693986fe6a12688fa751d11a94ce6
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-12-23 21:00:17 -0600

    list of products with angular

--- inventorie-app/src/app/product-list/product-list.component.ts
+++ inventorie-app/src/app/product-list/product-list.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { Product } from '../product';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-list',
-  standalone: true,
-  imports: [],
-  templateUrl: './product-list.component.html',
+  templateUrl: './product-list.component.html'
 })
 export class ProductListComponent {
+  products: Product[];
 
+  constructor(private productService: ProductService){}
+  
+  ngOnInit(){
+    //Cargamos los productos
+    this.obtainProducts();
+  }
+
+  private obtainProducts(){
+    // Consumir los datos del observable (suscribirnos)
+    this.productService.obtainProductsList().subscribe(
+      (data => {
+        this.products = data;
+      })
+    );
+  }
 }
